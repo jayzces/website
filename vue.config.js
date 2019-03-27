fceab82696747a3ce3ede107a634f72bbcd6ed17
Mary Louise Hermosa <jayzces@gmail.com>
2019-03-28 00:57:36 +0800

    Moving around paths

--- vue.config.js
+++ vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
             // swSrc is required in InjectManifest mode.
-            swSrc: 'public/service-worker.js',
+            swSrc: 'src/service-worker.js',
             // ...other Workbox options...
         }
     }
