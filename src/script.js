49a8c4f39a8e1117244759ffd7db2e8f5bd79694
Mary Louise Hermosa <jayzces@gmail.com>
2019-03-28 01:07:36 +0800

    Finally

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './components/app.vue'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,27 +1,10 @@
-import { register } from 'register-service-worker'
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-if (process.env.NODE_ENV === 'production') {
-    register(`${process.env.BASE_URL}service-worker.js`, {
-        ready () {
-            console.log('App is being served from cache by a service worker.\n' + 'For more details, visit https://goo.gl/AFskqB')
-        },
-        registered () {
-            console.log('Service worker has been registered.')
-        },
-        cached () {
-            console.log('Content has been cached for offline use.')
-        },
-        updatefound () {
-            console.log('New content is downloading.')
-        },
-        updated () {
-            console.log('New content is available; please refresh.')
-        },
-        offline () {
-            console.log('No internet connection found. App is running in offline mode.')
-        },
-        error (error) {
-            console.error('Error during service worker registration:', error)
-        }
-    })
-}
+// install new service worker when ok, then reload page.
+self.addEventListener("message", msg=>{
+    if (msg.data.action=='skipWaiting'){
+        self.skipWaiting()
+    }
+})
