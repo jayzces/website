8c2b1b7a81a53fbbff2a19bd1f775a350a895e4e
Mary Louise Hermosa <louise@channelfix.com>
2019-03-29 16:44:34 +0800

    Clean up

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,8 +3,6 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 // install new service worker when ok, then reload page.
-self.addEventListener("message", msg=>{
-    if (msg.data.action=='skipWaiting'){
-        self.skipWaiting()
-    }
+self.addEventListener("message", msg => {
+    if (msg.data.action == 'skipWaiting') self.skipWaiting()
 })
--- vue.config.js
+++ vue.config.js
@@ -16,24 +16,10 @@ module.exports = {
         svgRule.use('vue-svg-loader').loader('vue-svg-loader');
     },
     pwa: {
-        name: "HML",
-        themeColor: "#1d2930",
-        msTileColor: "#1D2930",
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: '#1D2930',
         manifestPath: 'site.webmanifest',
-        iconPaths: {
-            favicon32: 'icons/favicon-32x32.png',
-            favicon16: 'icons/favicon-16x16.png',
-            appleTouchIcon: 'icons/apple-touch-icon-152x152.png',
-            maskIcon: 'icons/safari-pinned-tab.svg',
-            msTileImage: 'icons/mstile-144x144.png'
-        },
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
-            // swSrc is required in InjectManifest mode.
             swSrc: 'src/service-worker.js',
-            // ...other Workbox options...
         }
     }
 }
